98d170510c248cea0f2f0bbf3d74722fdc820e81
4G0NYY <service@agony.ch>
2024-09-16 10:46:08 +0200

    PATH correction
    changed the path for the logs to a relative one

--- hello.py
+++ hello.py
@@ -28,6 +28,7 @@ Hello!!
 Still a comment!
 
 def hello():
+    print ("Hello!")
 
 HAHA got you, still just a comment :3
 '''
@@ -38,7 +39,7 @@ def hello():
 def debuglogorsmth():
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    with open("C:/Users/ramon/Documents/GitHub/RandomKackScript/Logs/logs.txt", "a") as file:
+    with open("Logs/logs.txt", "a") as file:
         file.write(formatted_time + "\n")
 
 if __name__ == '__main__':
